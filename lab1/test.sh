0c4be1ff9abf3c3a965fbf44c7228581308244e6
Fran Zekan <zekan.fran369@gmail.com>
2021-03-28 19:53:01 +0200

    Add more features to test script

--- lab1/test.sh
+++ lab1/test.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
-for i in {1..20}
-do
-  dir=$(printf "%0*d\n" 2 $i)
-  echo "Test $dir"
+raw_count="$(ls -l ./tests | wc -l)"
+count="$(($raw_count - 2))"
 
-  res=`python3 main.py < tests/test$dir/test.a | diff tests/test$dir/test.b -`
+if [ -z "$1" ]
+then
+  errors=""
+  error_count=0
+
+  for i in $(seq 0 $count)
+  do
+    dir=$(printf "%0*d\n" 2 $i)
+    echo "Test $dir"
+
+    res=`python3 main.py < tests/test$dir/test.a | diff tests/test$dir/test.b -`
+
+    if [ "$res" != "" ]
+    then
+      echo "Fail"
+      echo $res
+
+      error_count="$(($error_count + 1))"
+
+      if [ -z "$errors" ]
+      then
+        errors+="$dir"
+      else
+        errors+=", $dir"
+      fi
+    else
+      echo "Ok"
+    fi
+  done
+
+  echo "Errors: $errors"
+  echo "Completed: $(($count-$error_count+1))/$(($count + 1))"
+elif [ -z "$2" ]
+then
+  echo "Test $1"
+  res=`python3 main.py < tests/test$1/test.a | diff tests/test$1/test.b -`
 
   if [ "$res" != "" ]
   then
@@ -14,4 +47,12 @@ do
   else
     echo "Ok"
   fi
-done
+else
+  echo "Test $1"
+
+  echo "My:"
+  echo `python3 main.py < tests/test$1/test.a`
+
+  echo "Their:"
+  echo `cat tests/test$1/test.b`
+fi
