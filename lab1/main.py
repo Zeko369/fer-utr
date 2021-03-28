a6f5ab1d26716815e7f7bc6efca24f5aee206978
Fran Zekan <zekan.fran369@gmail.com>
2021-03-28 21:06:37 +0200

    lab[1]: Finish

--- lab1/main.py
+++ lab1/main.py
@@ -1,14 +1,16 @@
 from sys import stdin
-from pprint import pprint
 
 
-def add_rec(changes, key, prev=set()):
+def add_rec(changes, key, prev=set(), seen=set()):
+    if key in seen:
+        return prev
     prev.add(key)
+    seen.add(key)
+
     if key != '#' and '$' in changes[key].keys():
         prev.update(changes[key]['$'])
         for a in changes[key]['$']:
-            if key not in changes[key]['$']:
-                prev.update(add_rec(changes, a, prev))
+            prev.update(add_rec(changes, a, prev, seen))
     return prev
 
 
@@ -18,28 +20,21 @@ def main():
 
     _ = input() + input()
     start = input()
-
     for line in stdin:
         before, after = line.split('->')
         state, key = before.split(',')
-
         changes[state][key] = after.strip().split(',')
 
     for case in cases:
-        output = [add_rec(changes, start)]
+        output = [add_rec(changes, start, set(), set())]
         for option in case:
             output.append(set())
 
             for o in output[-2]:
-                if o == '#' or option not in changes[o].keys():
-                    continue  # o == '#' is same as o not in keys
-
-                tmp = changes[o][option]
-                for t in tmp:
-                    if t != '#' and '$' in changes[t].keys():
-                        output[-1].update(changes[t]['$'])
-
-                output[-1].update(tmp)
+                if o != '#' and option in changes[o].keys():
+                    output[-1].update(changes[o][option])
+                    for t in changes[o][option]:
+                        output[-1].update(add_rec(changes, t, set(), set()))
 
         output = map(lambda y: set(filter(lambda x: x != '#', y)), output)
         output = map(lambda x: ','.join(sorted(x) or ['#']), output)
