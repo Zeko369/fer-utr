858ffe189c042c8c4171669ef271d3b24f4e5583
Fran Zekan <zekan.fran369@gmail.com>
2021-03-28 20:26:53 +0200

    lab[1]: init whole changes to have a default value

--- lab1/main.py
+++ lab1/main.py
@@ -4,7 +4,7 @@ from pprint import pprint
 
 def add_rec(changes, key, prev=set()):
     prev.add(key)
-    if key in changes.keys() and '$' in changes[key].keys():
+    if key != '#' and '$' in changes[key].keys():
         prev.update(changes[key]['$'])
         for a in changes[key]['$']:
             if key not in changes[key]['$']:
@@ -14,18 +14,15 @@ def add_rec(changes, key, prev=set()):
 
 def main():
     cases = list(map(lambda x: x.split(','), input().split('|')))
-    _states = input().split(',')
-    _symbols = input().split(',')
-    _ok_states = input().split(',')
+    changes = {key: {'$': ['#']} for key in input().strip().split(',')}
+
+    _ = input() + input()
     start = input()
 
-    changes = {}
     for line in stdin:
         before, after = line.split('->')
         state, key = before.split(',')
 
-        if state not in changes.keys():
-            changes[state] = {}
         changes[state][key] = after.strip().split(',')
 
     for case in cases:
@@ -34,18 +31,17 @@ def main():
             output.append(set())
 
             for o in output[-2]:
-                if o not in changes.keys() or option not in changes[o].keys():
-                    continue
+                if o == '#' or option not in changes[o].keys():
+                    continue  # o == '#' is same as o not in keys
 
                 tmp = changes[o][option]
                 for t in tmp:
-                    if t in changes.keys() and '$' in changes[t].keys():
+                    if t != '#' and '$' in changes[t].keys():
                         output[-1].update(changes[t]['$'])
 
                 output[-1].update(tmp)
 
-            output[-1] = set(filter(lambda x: x != '#', output[-1]))
-
+        output = map(lambda y: set(filter(lambda x: x != '#', y)), output)
         output = map(lambda x: ','.join(sorted(x) or ['#']), output)
         print('|'.join(output))
 
